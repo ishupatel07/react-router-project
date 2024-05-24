8f177c870743ad90ad32b6ca5e0dfb831846698e
Ishwar Patel <ishwarrpatel22@gmail.com>
2024-05-24 10:34:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header/Header.jsx'
+import Home from './components/Home/Home.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default App
